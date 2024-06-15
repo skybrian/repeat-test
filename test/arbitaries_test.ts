2fc9bc29619f16ae3c67f7ae4c7e9132357a2cd9
Brian Slesinsky <brian-git@slesinsky.org>
2024-06-14 22:46:10 -0700

    start testing arb.record

--- test/arbitaries_test.ts
+++ test/arbitaries_test.ts
@@ -104,3 +104,37 @@ describe("oneOf", () => {
     checkParses(threeWay, [2, 5], 5);
   });
 });
+
+describe("record", () => {
+  describe("for an empty record shape", () => {
+    it("returns it without needing a decision", () => {
+      checkParses(arb.record({}), [], {});
+    });
+  });
+  describe("for a constant record shape", () => {
+    const example = arb.record({
+      a: arb.example([1]),
+      b: arb.example([2]),
+    });
+    it("returns it without needing a decision", () => {
+      checkParses(example, [], { a: 1, b: 2 });
+    });
+  });
+  describe("for a record that requires a decision", () => {
+    const oneField = arb.record({
+      a: arb.chosenInt(1, 2),
+    });
+    it("defaults to using the default value of the field", () => {
+      checkParseFails(oneField, [], { a: 1 }, 0);
+    });
+  });
+  describe("for a record that requires multiple decisions", () => {
+    const example = arb.record({
+      a: arb.chosenInt(1, 2),
+      b: arb.chosenInt(3, 4),
+    });
+    it("reads decisions ordered by its keys", () => {
+      checkParses(example, [1, 3], { a: 1, b: 3 });
+    });
+  });
+});
