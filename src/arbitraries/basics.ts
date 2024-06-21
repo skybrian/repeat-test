24ca41c14c87d5fbc05eec0f3c5fca0a31c07701
Brian Slesinsky <brian-git@slesinsky.org>
2024-06-20 21:39:51 -0700

    set a limit on generated array lengths

--- src/arbitraries/basics.ts
+++ src/arbitraries/basics.ts
@@ -1,5 +1,16 @@
 import { Arbitrary, PickFunction } from "./core.ts";
-import { PickRequest } from "../picks.ts";
+import { chooseDefault, PickRequest, PickRequestOptions } from "../picks.ts";
+
+/**
+ * Defines an arbitrary that's based on a callback function that generates a
+ * value from a stream of picks.
+ *
+ * The callback must always succeed, but see {@link Arbitrary.filter} for a way
+ * to do backtracking in a subrequest.
+ */
+export function custom<T>(callback: (pick: PickFunction) => T) {
+  return new Arbitrary((pick) => callback(pick));
+}
 
 /**
  * An integer range, to be picked from uniformly.
@@ -15,27 +26,15 @@ export function uniformInt(
   return new Arbitrary((pick) => pick(req));
 }
 
-/**
- * An integer range, to be picked from with bias towards special cases.
- */
-export function int(
-  min: number,
-  max: number,
-  opts?: { default?: number },
-): Arbitrary<number> {
-  if (max - min <= 10) {
-    return uniformInt(min, max, opts);
-  }
-  function pickBiased(
-    uniform: (min: number, max: number) => number,
-  ): number {
+function specialNumberBias(min: number, max: number, defaultVal: number) {
+  function pick(uniform: (min: number, max: number) => number): number {
     switch (uniform(0, 15)) {
       case 0:
-        return req.min;
+        return min;
       case 1:
-        return req.max;
+        return max;
       case 2:
-        return req.default;
+        return defaultVal;
       case 3:
         if (min <= 0 && max >= 0) return 0;
         break;
@@ -47,20 +46,26 @@ export function int(
     }
     return uniform(min, max);
   }
-
-  const req = new PickRequest(min, max, { ...opts, bias: pickBiased });
-  return new Arbitrary((pick) => pick(req));
+  return pick;
 }
 
 /**
- * Defines an arbitrary that's based on a callback function that generates a
- * value from a stream of picks.
- *
- * The callback must always succeed, but see {@link Arbitrary.filter} for a way
- * to do backtracking in a subrequest.
+ * An integer range, to be picked from with bias towards special cases.
  */
-export function custom<T>(callback: (pick: PickFunction) => T) {
-  return new Arbitrary((pick) => callback(pick));
+export function int(
+  min: number,
+  max: number,
+  opts?: { default?: number },
+): Arbitrary<number> {
+  if (max - min <= 10) {
+    return uniformInt(min, max, opts);
+  }
+
+  const defaultVal = chooseDefault(min, max, opts);
+  const bias = specialNumberBias(min, max, defaultVal);
+  const req = new PickRequest(min, max, { default: defaultVal, bias });
+
+  return new Arbitrary((pick) => pick(req));
 }
 
 export function example<T>(values: T[]): Arbitrary<T> {
@@ -85,16 +90,38 @@ export function oneOf<T>(alternatives: Arbitrary<T>[]): Arbitrary<T> {
   return example(alternatives).chain((chosen) => chosen);
 }
 
+/**
+ * An arbitrary but practical limit for maximum array length.
+ * (Deno becomes slow at 4e7 and crashes above that for a filled array.)
+ */
+const maxArrayLength = 1e7;
+
+/** Returns a random integer that's a suitable length for an array. */
+export function length(
+  min: number,
+  max: number,
+  opts?: PickRequestOptions,
+): Arbitrary<number> {
+  if (min < 0) {
+    throw new Error("min must be non-negative");
+  }
+  if (max > maxArrayLength) {
+    throw new Error("max is too large");
+  }
+  const req = new PickRequest(min, max, opts);
+  return new Arbitrary((pick) => pick(req));
+}
+
 export function array<T>(
   item: Arbitrary<T>,
   opts?: { min?: number; max?: number },
 ): Arbitrary<T[]> {
-  const minLength = opts?.min ?? 0;
-  const maxLength = opts?.max ?? 10;
+  const min = opts?.min ?? 0;
+  const max = opts?.max ?? 10;
   return custom((pick) => {
-    const length = pick(int(minLength, maxLength));
+    const len = pick(length(min, max));
     const result: T[] = [];
-    for (let i = 0; i < length; i++) {
+    for (let i = 0; i < len; i++) {
       result.push(pick(item));
     }
     return result;
