a6e8611d2d28b81a4927dc43cb050d5682a98981
Brian Slesinsky <brian-git@slesinsky.org>
2024-06-25 18:34:41 -0700

    make Arbitrary class tests more self-contained

--- test/arbitrary_class_test.ts
+++ test/arbitrary_class_test.ts
@@ -1,15 +1,11 @@
 import { describe, it } from "@std/testing/bdd";
 import { assert, assertEquals, assertThrows } from "@std/assert";
-import { repeatTest } from "../../src/runner.ts";
-import * as arb from "../../src/arbitraries.ts";
+import { repeatTest } from "../src/runner.ts";
 
-import { PickRequest } from "../../src/picks.ts";
-import { NestedPicks } from "../../src/playouts.ts";
-import { PlayoutFailed } from "../../src/solver.ts";
-import Arbitrary from "../../src/arbitrary_class.ts";
-
-const oneToSix = new PickRequest(1, 6);
-const sixSided = new Arbitrary((pick) => pick(oneToSix));
+import { PickRequest } from "../src/picks.ts";
+import { NestedPicks } from "../src/playouts.ts";
+import { PlayoutFailed } from "../src/solver.ts";
+import Arbitrary from "../src/arbitrary_class.ts";
 
 describe("Arbitrary", () => {
   describe("constructor", () => {
@@ -21,6 +17,9 @@ describe("Arbitrary", () => {
     });
   });
   describe("filter", () => {
+    const oneToSix = new PickRequest(1, 6);
+    const sixSided = new Arbitrary((pick) => pick(oneToSix));
+
     it("disallows filters that doesn't accept the default", () => {
       const rejectEverything = () => false;
       assertThrows(() => sixSided.filter(rejectEverything));
@@ -29,7 +28,7 @@ describe("Arbitrary", () => {
       const not3 = sixSided.filter((n) => n !== 3);
       repeatTest(not3, (n) => {
         assert(n !== 3);
-      }, { only: "1220797316:10" });
+      });
     });
   });
   describe("map", () => {
@@ -43,19 +42,23 @@ describe("Arbitrary", () => {
     });
   });
   describe("members", () => {
-    it("returns the only member of a constant Arbitrary", () => {
+    it("returns the only value of a constant", () => {
       const one = new Arbitrary(() => 1);
       assertEquals(Array.from(one.members), [1]);
     });
-    it("returns each example from a boolean", () => {
-      const members = Array.from(arb.boolean().members);
-      assertEquals(members, [false, true]);
-    });
-    it("handles a mapped Arbitrary", () => {
-      const bit = arb.boolean().map((b) => b ? 1 : 0);
+
+    const bit = new Arbitrary((pick) => pick(new PickRequest(0, 1)));
+    it("returns each example of a bit", () => {
       const members = Array.from(bit.members);
       assertEquals(members, [0, 1]);
     });
+
+    const boolean = bit.map((b) => b == 1);
+    it("handles a mapped Arbitrary", () => {
+      const members = Array.from(boolean.members);
+      assertEquals(members, [false, true]);
+    });
+
     it("handles PlayoutFailed", () => {
       const onlyThree = new Arbitrary((pick) => {
         const n = pick(new PickRequest(2, 3, { default: 3 }));
@@ -64,23 +67,29 @@ describe("Arbitrary", () => {
       });
       assertEquals(Array.from(onlyThree.members), [3]);
     });
+
     it("handles a filtered Arbitrary", () => {
-      const justFalse = arb.boolean().filter((b) => !b);
+      const justFalse = boolean.filter((b) => !b);
       assertEquals(Array.from(justFalse.members), [false]);
     });
+
     it("handles a chained Arbitrary", () => {
-      const len = arb.int(0, 1);
-      const string = len.chain((len) =>
-        arb.array(arb.example(["hi", "there"]), { min: len, max: len })
-      );
-      assertEquals(Array.from(string.members), [[], ["hi"], ["there"]]);
+      const hello = boolean.chain((val) => {
+        if (val) {
+          return new Arbitrary(() => "there");
+        } else {
+          return new Arbitrary(() => "hi");
+        }
+      });
+      assertEquals(Array.from(hello.members), ["hi", "there"]);
     });
     it("can solve a combination lock", () => {
-      const digits = arb.tuple(
-        arb.int(0, 9, { default: 1 }),
-        arb.int(0, 9, { default: 4 }),
-        arb.int(0, 9, { default: 3 }),
-      );
+      const digits = new Arbitrary((pick) => {
+        const a = pick(new PickRequest(0, 9, { default: 1 }));
+        const b = pick(new PickRequest(0, 9, { default: 4 }));
+        const c = pick(new PickRequest(0, 9, { default: 3 }));
+        return [a, b, c];
+      });
       const lock = digits.filter(([a, b, c]) =>
         a == 1 && (b == 2 || b == 4) && c == 3
       );
@@ -103,21 +112,29 @@ describe("Arbitrary", () => {
 
   describe("solutions", () => {
     it("returns the only solution for a constant", () => {
-      checkSolutions(arb.just(1), [{ val: 1, picks: [] }]);
+      const one = new Arbitrary(() => 1);
+      checkSolutions(one, [{ val: 1, picks: [] }]);
     });
     it("returns each solution for an int range", () => {
-      checkSolutions(arb.int(1, 3), [
+      const oneTwoThree = new Arbitrary((pick) => {
+        return pick(new PickRequest(1, 3));
+      });
+      checkSolutions(oneTwoThree, [
         { val: 1, picks: [1] },
         { val: 2, picks: [2] },
         { val: 3, picks: [3] },
       ]);
     });
     it("returns each solution for a boolean", () => {
+      const bit = new Arbitrary((pick) => {
+        return pick(new PickRequest(0, 1));
+      });
+      const boolean = bit.map((b) => b == 1);
       const expected = [
         { val: false, picks: [[0]] },
         { val: true, picks: [[1]] },
       ];
-      checkSolutions(arb.boolean(), expected);
+      checkSolutions(boolean, expected);
     });
   });
 });
