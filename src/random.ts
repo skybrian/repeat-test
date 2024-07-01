8aae0ed0421a8d7f9e2d0d1af4dae74bb5fe0bf6
Brian Slesinsky <brian-git@slesinsky.org>
2024-07-01 15:48:27 -0700

    random returns a retry picker

--- src/random.ts
+++ src/random.ts
@@ -1,5 +1,5 @@
 import prand from "pure-rand";
-import { IntPicker, PickRequest, UniformIntPicker } from "./picks.ts";
+import { PickRequest, RetryPicker, UniformIntPicker } from "./picks.ts";
 
 export function pickRandomSeed(): number {
   return Date.now() ^ (Math.random() * 0x100000000);
@@ -9,16 +9,26 @@ export function pickRandomSeed(): number {
  * Creates a random picker that uses the RandomGenerator's current state as a
  * starting point.
  */
-function makePicker(rng: prand.RandomGenerator): IntPicker {
+function makePicker(rng: prand.RandomGenerator): RetryPicker {
   // Clone so we can mutate it.
   rng = rng.clone();
 
   const uniform: UniformIntPicker = (min, max) =>
     prand.unsafeUniformIntDistribution(min, max, rng);
 
+  const picks: number[] = [];
+
   return {
     pick(req: PickRequest) {
-      return req.bias(uniform);
+      const pick = req.bias(uniform);
+      picks.push(pick);
+      return pick;
+    },
+    depth: picks.length,
+    getPicks: () => picks.slice(),
+    replaying: false,
+    backTo: () => {
+      return true;
     },
   };
 }
@@ -26,7 +36,7 @@ function makePicker(rng: prand.RandomGenerator): IntPicker {
 /**
  * Returns a single random number generator.
  */
-export function randomPicker(seed: number): IntPicker {
+export function randomPicker(seed: number): RetryPicker {
   return makePicker(prand.xoroshiro128plus(seed));
 }
 
@@ -41,7 +51,7 @@ function jump(r: prand.RandomGenerator): prand.RandomGenerator {
 /**
  * Returns a sequence of random number generators where each is independent.
  */
-export function* randomPickers(seed: number): IterableIterator<IntPicker> {
+export function* randomPickers(seed: number): IterableIterator<RetryPicker> {
   let rng = prand.xoroshiro128plus(seed);
   while (true) {
     yield makePicker(rng);
