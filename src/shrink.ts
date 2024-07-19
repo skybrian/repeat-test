6bcad4742c67d0c46ed6cf346a6a1dc91ef8d75a
Brian Slesinsky <brian-git@slesinsky.org>
2024-07-18 22:54:43 -0700

    got shrinking working for simple cases

--- src/shrink.ts
+++ src/shrink.ts
@@ -1,18 +1,62 @@
-import { PickRequest } from "./picks.ts";
+import { PickRequest, PlaybackPicker } from "./picks.ts";
+import { onePlayout } from "./backtracking.ts";
 import { Playout } from "./playouts.ts";
+import Arbitrary, { Solution } from "./arbitrary_class.ts";
 
-/** Returns a list of guesses that are smaller than the given reply. */
-export function pickGuesses(
-  req: PickRequest,
-  reply: number,
-): Iterable<number> {
-  if (reply === req.min) {
-    return [];
+/**
+ * Given a playout from an arbitrary, returns a smaller solution that satisfies a predicate.
+ */
+export function shrink<T>(
+  arb: Arbitrary<T>,
+  sol: Solution<T>,
+  interesting: (arg: T) => boolean,
+): Solution<T> {
+  while (true) {
+    const next = shrinkOnce(arb, guesses(sol.playout), interesting);
+    if (!next) {
+      return sol;
+    }
+    sol = next;
   }
-  return [req.min];
 }
 
-export function* removeTrailingGuesses(
+export function shrinkOnce<T>(
+  arb: Arbitrary<T>,
+  guesses: Iterable<number[]>,
+  interesting: (arg: T) => boolean,
+): Solution<T> | undefined {
+  for (const guess of guesses) {
+    const picker = new PlaybackPicker(guess);
+    const input = arb.pickSolution(onePlayout(picker));
+    if (input && interesting(input.val)) {
+      return input;
+    }
+  }
+  return undefined;
+}
+
+function* guesses(
+  playout: Playout,
+): Iterable<number[]> {
+  const shorter = shorterGuesses(playout)[Symbol.iterator]();
+  const smaller = changePickGuesses(playout)[Symbol.iterator]();
+  while (true) {
+    const next1 = shorter.next();
+    const next2 = smaller.next();
+    if (next1.done && next2.done) {
+      return;
+    }
+    if (!next1.done) {
+      yield next1.value;
+    }
+    if (!next2.done) {
+      yield next2.value;
+    }
+  }
+}
+
+/** Tries removing suffixes of the given playout. */
+export function* shorterGuesses(
   playout: Playout,
 ): Iterable<number[]> {
   const picks = playout.picks.trim();
@@ -21,12 +65,38 @@ export function* removeTrailingGuesses(
   }
 
   // Try trimming the last half of the picks.
-  if (picks.length > 1) {
-    let newLen = Math.ceil(picks.length / 2);
+  if (picks.length > 0) {
+    let newLen = Math.floor(picks.length / 2);
     while (newLen < picks.length) {
       yield picks.replies.slice(0, newLen);
       const remaining = picks.length - newLen;
       newLen += Math.ceil(remaining / 2);
     }
+  }
+}
+
+/** Tries changing one pick at a time. */
+export function* changePickGuesses(
+  playout: Playout,
+): Iterable<number[]> {
+  const picks = playout.picks.trim();
+  const { reqs, replies } = playout.picks;
+  for (let i = 0; i < picks.length; i++) {
+    for (const guess of pickGuesses(reqs[i], replies[i])) {
+      yield [...replies.slice(0, i), guess, ...replies.slice(i + 1)];
+    }
+  }
+}
+
+/** Returns a list of guesses that are smaller than the given reply. */
+export function* pickGuesses(
+  req: PickRequest,
+  reply: number,
+): Iterable<number> {
+  let min = req.min;
+  while (min < reply) {
+    const guess = Math.floor((min + reply) / 2);
+    yield guess;
+    min = guess + 1;
   }
 }
--- test/shrink_test.ts
+++ test/shrink_test.ts
@@ -4,33 +4,62 @@ import * as arb from "../src/arbitraries.ts";
 import { repeatTest } from "../src/runner.ts";
 
 import { PickRequest } from "../src/picks.ts";
-import { pickGuesses, removeTrailingGuesses } from "../src/shrink.ts";
+import {
+  changePickGuesses,
+  pickGuesses,
+  shorterGuesses,
+  shrink,
+} from "../src/shrink.ts";
 import { Playout } from "../src/playouts.ts";
+import Arbitrary from "../src/arbitrary_class.ts";
 
-describe("pickGuesses", () => {
-  it("doesn't guess when already at the minimum", () => {
-    const req = new PickRequest(1, 6);
-    const guesses = Array.from(pickGuesses(req, 1));
-    assertEquals(guesses, []);
+function assertShrink<T>(
+  choices: Arbitrary<T>,
+  interesting: (arg: T) => boolean,
+  start: T,
+  result: T,
+) {
+  const startSol = choices.findSolution((val) => val === start);
+  assert(startSol, "didn't find starting solution");
+
+  const smaller = shrink(choices, startSol, interesting);
+  assert(smaller, "didn't find a smaller solution");
+  assertEquals(smaller.val, result);
+}
+
+describe("shrink", () => {
+  describe("for a single pick", () => {
+    it("returns the same solution when already at the minimum", () => {
+      assertShrink(arb.int(1, 6), () => true, 1, 1);
+    });
+    it("shrinks an int to the minimum", () => {
+      assertShrink(arb.int(1, 6), () => true, 6, 1);
+    });
+    it("finds a smaller int", () => {
+      assertShrink(arb.int(1, 6), (n) => n >= 3, 6, 3);
+    });
   });
-  it("guesses the minimum value", () => {
-    const req = new PickRequest(1, 6);
-    const guesses = Array.from(pickGuesses(req, 6));
-    assertEquals(guesses, [1]);
+  describe("for an ascii letter", () => {
+    it("returns 'a' when given 'a'", () => {
+      assertShrink(arb.asciiLetter(), () => true, "a", "a");
+    });
+    it("shrinks 'Z' to 'a'", () => {
+      assertShrink(arb.asciiLetter(), () => true, "z", "a");
+    });
   });
 });
 
-describe("removeTrailingGuesses", () => {
+describe("shorterGuesses", () => {
   it("doesn't guess for an empty playout", () => {
     const playout = new Playout([]);
-    const guesses = Array.from(removeTrailingGuesses(playout));
+    const guesses = Array.from(shorterGuesses(playout));
     assertEquals(guesses, []);
   });
   it("doesn't guess if no requests were provided", () => {
     const example = arb.array(arb.int(0, 1000));
     repeatTest(example, (picks) => {
       const playout = new Playout(picks);
-      const guesses = Array.from(removeTrailingGuesses(playout));
+      const guesses = Array.from(shorterGuesses(playout));
       assertEquals(guesses, []);
     });
   });
@@ -40,7 +69,7 @@ describe("removeTrailingGuesses", () => {
       const reqs = ranges.map((r) => new PickRequest(r.min, r.max));
       const picks = ranges.map((r) => r.min);
       const playout = new Playout(picks, { reqs });
-      const guesses = Array.from(removeTrailingGuesses(playout));
+      const guesses = Array.from(shorterGuesses(playout));
       assertEquals(guesses, []);
     });
   });
@@ -58,7 +87,7 @@ describe("removeTrailingGuesses", () => {
     repeatTest(playout, ({ ranges, picks }) => {
       const reqs = ranges.map((r) => new PickRequest(r.min, r.max));
       const playout = new Playout(picks, { reqs });
-      const guesses = Array.from(removeTrailingGuesses(playout));
+      const guesses = Array.from(shorterGuesses(playout));
       assert(guesses.length > 0);
 
       // The last guess should remove one pick.
@@ -76,5 +105,34 @@ describe("removeTrailingGuesses", () => {
         prevSize = guess.length;
       }
     });
+  });
+});
+
+describe("changePickGuesses", () => {
+  it("doesn't guess for an empty playout", () => {
+    const playout = new Playout([]);
+    const guesses = Array.from(changePickGuesses(playout));
+    assertEquals(guesses, []);
+  });
+  it("replaces one pick with the minimum", () => {
+    const roll = new PickRequest(1, 2);
+    const playout = new Playout([2, 2], {
+      reqs: [roll, roll],
+    });
+    const guesses = Array.from(changePickGuesses(playout));
+    assertEquals(guesses, [[1, 2], [2, 1]]);
+  });
+});
+
+describe("pickGuesses", () => {
+  it("doesn't guess when already at the minimum", () => {
+    const req = new PickRequest(1, 6);
+    const guesses = Array.from(pickGuesses(req, 1));
+    assertEquals(guesses, []);
+  });
+  it("guesses smaller values", () => {
+    const req = new PickRequest(1, 7);
+    const guesses = Array.from(pickGuesses(req, 7));
+    assertEquals(guesses, [4, 6]);
   });
 });
