76fd994f7b9fe4f036f035a2cc98178f6994bc4e
Brian Slesinsky <brian-git@slesinsky.org>
2024-07-24 14:12:27 -0700

    implement uniqueArray

--- src/arbitraries/tables.ts
+++ src/arbitraries/tables.ts
@@ -2,6 +2,21 @@ import { AnyRecord } from "../types.ts";
 import Arbitrary, { RecordShape } from "../arbitrary_class.ts";
 import * as arb from "./basics.ts";
 
+export function uniqueArray<T>(
+  item: Arbitrary<T>,
+  opts?: { label?: string },
+): Arbitrary<T[]> {
+  const label = opts?.label ?? "uniqueArray";
+  return arb.from((pick) => {
+    const items = new Set<T>();
+    const filtered = item.filter((v) => !items.has(v));
+    while (pick(arb.boolean())) {
+      items.add(pick(filtered));
+    }
+    return Array.from(items);
+  }, { label });
+}
+
 export type TableOpts<T extends AnyRecord> = {
   label?: string;
 };
--- test/arbitraries/tables_test.ts
+++ test/arbitraries/tables_test.ts
@@ -2,6 +2,30 @@ import { describe, it } from "@std/testing/bdd";
 import { assertEquals } from "@std/assert";
 
 import * as arb from "../../src/arbitraries.ts";
+import { assertValues } from "../../src/asserts.ts";
+
+describe("uniqueArray", () => {
+  const bools = arb.uniqueArray(arb.boolean());
+  it("defaults to an empty array", () => {
+    assertEquals(bools.default(), []);
+  });
+  it("generates all combinations of a boolean", () => {
+    assertValues(bools, [
+      [],
+      [false],
+      [true],
+      [true, false],
+      [false, true],
+    ]);
+  });
+  it("has a label", () => {
+    assertEquals(bools.label, "uniqueArray");
+  });
+  it("can be configured with a label", () => {
+    const array = arb.uniqueArray(arb.int(1, 3), { label: "my array" });
+    assertEquals(array.label, "my array");
+  });
+});
 
 describe("table", () => {
   const table = arb.table({
