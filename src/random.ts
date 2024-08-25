2b4ad12cb97d658190b38272d8acf0a23fdf6b05
Brian Slesinsky <brian-git@slesinsky.org>
2024-08-25 13:15:15 -0700

    tidy: introduce uniformSource function

--- src/random.ts
+++ src/random.ts
@@ -6,24 +6,20 @@ export function pickRandomSeed(): number {
   return Date.now() ^ (Math.random() * 0x100000000);
 }
 
+export function uniformSource(rng: prand.RandomGenerator): UniformRandomSource {
+  return (min, max) => prand.unsafeUniformIntDistribution(min, max, rng);
+}
+
 /**
  * Creates a random picker that uses the RandomGenerator's current state as a
  * starting point.
  */
 function makePicker(rng: prand.RandomGenerator): IntPicker {
-  // Clone so we can mutate it.
-  rng = rng.clone();
-
-  const uniform: UniformRandomSource = (min, max) =>
-    prand.unsafeUniformIntDistribution(min, max, rng);
-
-  const picks: number[] = [];
+  const uniform = uniformSource(rng.clone());
 
   return {
     pick(req: PickRequest) {
-      const pick = req.bias(uniform);
-      picks.push(pick);
-      return pick;
+      return req.bias(uniform);
     },
   };
 }
