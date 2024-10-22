84fc4f16262a2d8f36af397b4d2519846a0777ab
Brian Slesinsky <brian-git@slesinsky.org>
2024-10-21 18:19:27 -0700

    tidy: move shrink tests after Shrinker tests

--- test/shrink_test.ts
+++ test/shrink_test.ts
@@ -66,242 +66,6 @@ const acceptAll = () => true;
 const bitReq = new PickRequest(0, 1);
 const roll = new PickRequest(1, 6);
 
-describe("shrink", () => {
-  describe("for an int", () => {
-    it("can't shrink the minimum value", () => {
-      assertNoChange(dom.int(1, 6), () => true, 1);
-    });
-    it("can't shrink when the value is required", () => {
-      repeatTest(minMaxVal(), ({ min, max, val }) => {
-        assertNoChange(dom.int(min, max), (n) => n === val, val);
-      });
-    });
-    it("shrinks an unused positive int to the minimum", () => {
-      assertShrinks(dom.int(1, 6), () => true, 6, 1);
-    });
-    it("shrinks an unused negative int to the maximum", () => {
-      assertShrinks(dom.int(-6, -1), () => true, -6, -1);
-    });
-    it("shrinks as far as possible for an inequality", () => {
-      assertShrinks(dom.int(1, 6), (n) => n >= 3, 6, 3);
-    });
-  });
-
-  describe("for an int32", () => {
-    it("shrinks to a positive number", () => {
-      assertShrinks(dom.int32(), (n) => n >= 10, 12345, 10);
-    });
-  });
-
-  describe("for an ascii character", () => {
-    it("can't shrink 'a'", () => {
-      assertNoChange(dom.asciiChar(), () => true, "a");
-    });
-    it("can't shrink when all characters are used", () => {
-      repeatTest(arb.asciiChar(), (start) => {
-        assertNoChange(dom.asciiChar(), (c) => c === start, start);
-      });
-    });
-    it("shrinks an unused character to 'a'", () => {
-      assertShrinks(dom.asciiChar(), () => true, "Z", "a");
-    });
-    it("shrinks a used character to a lower one that works", () => {
-      assertShrinks(dom.asciiChar(), (s) => /[A-Z]/.test(s), "Z", "A");
-    });
-  });
-
-  describe("for a string", () => {
-    it("can't shrink an empty string", () => {
-      assertNoChange(dom.string(), () => true, "");
-    });
-    it("can't shrink when there's no alternative", () => {
-      repeatTest(arb.string(), (start) => {
-        assertNoChange(dom.string(), (s) => s === start, start);
-      }, { reps: 10 });
-    });
-    it("removes unused trailing characters", () => {
-      assertShrinks(dom.string(), (s) => s.startsWith("a"), "abc", "a");
-    });
-    it("sets unused characters to 'a'", () => {
-      assertShrinks(dom.string(), (s) => s.at(2) === "z", "xyz", "aaz");
-    });
-    it("removes unused leading characters", () => {
-      assertShrinks(dom.string(), (s) => s.endsWith("z"), "xyz", "z");
-    });
-  });
-
-  describe("for a sequence of options", () => {
-    const bits = arb.array(arb.int(0, 1), { length: { min: 1, max: 100 } });
-
-    it("removes any sequence of options", () => {
-      repeatTest(bits, (bits) => {
-        const seed = seedFrom(bits.map(() => bitReq), bits);
-        const gen = shrink(seed, acceptAll);
-        assert(gen !== undefined);
-        assertEquals(gen.picks.trimmed().replies, []);
-      });
-    });
-
-    it("removes everything except one option", () => {
-      const input = arb.from((pick) => {
-        const prefix = pick(bits);
-        const suffix = pick(bits);
-        return { prefix, suffix };
-      });
-      repeatTest(input, ({ prefix, suffix }) => {
-        const val = prefix.concat([1]).concat(suffix);
-        const seed = seedFrom(val.map((_) => bitReq), val);
-        const gen = shrink(seed, (arr) => arr.at(prefix.length) === 1);
-        assert(gen !== undefined);
-        const expected = Array(prefix.length).fill(0).concat(1);
-        assertEquals(gen.picks.trimmed().replies, expected);
-      });
-    });
-  });
-
-  describe("for a mult-step build script", () => {
-    const bit = Script.make("bit", (pick) => pick(PickRequest.bit));
-
-    const twoBits = bit.then("twoBits", (a, pick) => {
-      const b = pick(PickRequest.bit);
-      return [a, b];
-    });
-
-    it("can't shrink the default value", () => {
-      const seed = Gen.mustBuild(twoBits, [0, 0]);
-      const gen = shrink(seed, acceptAll);
-      assertEquals(gen.val, [0, 0]);
-    });
-
-    it("can shrink both bits to zero", () => {
-      const seed = Gen.mustBuild(twoBits, [1, 1]);
-      const gen = shrink(seed, acceptAll);
-      assertEquals(gen.val, [0, 0]);
-    });
-  });
-
-  function includesSubarray<T>(array: T[], subarray: T[]): boolean {
-    return array.some((_, i) =>
-      subarray.every((val, j) => val === array[i + j])
-    );
-  }
-
-  describe("for an array of ints", () => {
-    it("removes leading and trailing unused elements", () => {
-      const hay = arb.array(arb.int(1, 5), { length: { max: 10 } });
-      const example = arb.from((pick) => {
-        const prefix = pick(hay);
-        const suffix = pick(hay);
-        const needleSize = pick(arb.int(1, 3));
-        const needle = Array(needleSize).fill(6);
-        return { prefix, needle, suffix };
-      });
-      repeatTest(example, ({ prefix, needle, suffix }) => {
-        const input = prefix.concat(needle).concat(suffix);
-        assertShrinks(
-          dom.array(dom.int(1, 6)),
-          (a) => includesSubarray(a, needle),
-          input,
-          needle,
-        );
-      }, { reps: 100 });
-    });
-  });
-
-  describe("for an array of strings", () => {
-    it("removes all strings in an array", () => {
-      const input = arb.array(arb.string(), { length: { min: 1, max: 5 } });
-      repeatTest(input, (arr) => {
-        assertShrinks(dom.array(dom.string()), acceptAll, arr, []);
-      });
-    });
-
-    it("removes leading and trailing unused elements", () => {
-      const hayString = arb.string({ length: { max: 2 } });
-      const hay = arb.array(hayString, { length: { max: 10 } });
-      const example = arb.from((pick) => {
-        const prefix = pick(hay);
-        const suffix = pick(hay);
-        const needleSize = pick(arb.int(1, 3));
-        const needle = Array(needleSize).fill("<->");
-        return { prefix, needle, suffix };
-      });
-      repeatTest(example, ({ prefix, needle, suffix }) => {
-        const input = prefix.concat(needle).concat(suffix);
-        assert(!includesSubarray(prefix, needle));
-        assertShrinks(
-          dom.array(dom.string()),
-          (a) => includesSubarray(a, needle),
-          input,
-          needle,
-        );
-      }, { reps: 100 });
-    });
-
-    function repeatString(s: string, n: number): string {
-      let res = "";
-      for (let i = 0; i < n; i++) {
-        res += s;
-      }
-      return res;
-    }
-
-    it("shrinks each string to a given size", () => {
-      const input = arb.array(arb.string({ length: 20 }), { length: 10 });
-      const seed = generate(input, onePlayout(randomPicker(123)));
-      assert(seed !== filtered);
-      const gen = shrink(seed, () => true);
-      const expectedItem = repeatString("a", 20);
-      assertEquals(gen.val, Array(10).fill(expectedItem));
-    });
-  });
-
-  describe("for a filtered string", () => {
-    const filtered = dom.string().filter((s) =>
-      s.length > 0 && !s.startsWith("a")
-    );
-    it("can't shrink a default value", () => {
-      assertNoChange(filtered, () => true, "b");
-    });
-    it("shrinks non-default values", () => {
-      repeatTest(filtered.filter((s) => s !== "b"), (suffix, console) => {
-        assertShrinks(filtered, () => true, suffix, "b", console);
-      }, { reps: 100 });
-    });
-  });
-
-  describe("for a record", () => {
-    it("can't shrink an empty record", () => {
-      assertNoChange(dom.record({}), () => true, {});
-    });
-    const pair = dom.record({ a: dom.int32(), b: dom.string() });
-    it("can't shrink when there's no alternative", () => {
-      repeatTest(pair, ({ a, b }) => {
-        assertNoChange(
-          pair,
-          (r) => r.a === a && r.b === b,
-          { a, b },
-        );
-      }, { reps: 10 });
-    });
-    it("shrinks all fields to their minimums", () => {
-      repeatTest(pair, (start) => {
-        assertShrinks(pair, (_r) => true, start, { a: 0, b: "" });
-      }, { reps: 100 });
-    });
-    it("shrinks the first field if the second is held constant", () => {
-      repeatTest(pair, ({ a, b }) => {
-        assertShrinks(pair, (r) => r.b === b, { a, b }, { a: 0, b });
-      }, { reps: 10 });
-    });
-    it("shrinks the second field if the first is held constant", () => {
-      repeatTest(pair, ({ a, b }) => {
-        assertShrinks(pair, (r) => r.a === a, { a, b }, { a, b: "" });
-      }, { reps: 100 });
-    });
-  });
-});
-
 describe("Shrinker", () => {
   describe("shrinkTails", () => {
     function shrinkTails<T>(
@@ -542,6 +306,242 @@ describe("Shrinker", () => {
         const gen = shrinkOnePick(0, 0, seed, accept);
         assertEquals(gen?.replies, [want]);
       });
+    });
+  });
+});
+
+describe("shrink", () => {
+  describe("for an int", () => {
+    it("can't shrink the minimum value", () => {
+      assertNoChange(dom.int(1, 6), () => true, 1);
+    });
+    it("can't shrink when the value is required", () => {
+      repeatTest(minMaxVal(), ({ min, max, val }) => {
+        assertNoChange(dom.int(min, max), (n) => n === val, val);
+      });
+    });
+    it("shrinks an unused positive int to the minimum", () => {
+      assertShrinks(dom.int(1, 6), () => true, 6, 1);
+    });
+    it("shrinks an unused negative int to the maximum", () => {
+      assertShrinks(dom.int(-6, -1), () => true, -6, -1);
+    });
+    it("shrinks as far as possible for an inequality", () => {
+      assertShrinks(dom.int(1, 6), (n) => n >= 3, 6, 3);
+    });
+  });
+
+  describe("for an int32", () => {
+    it("shrinks to a positive number", () => {
+      assertShrinks(dom.int32(), (n) => n >= 10, 12345, 10);
+    });
+  });
+
+  describe("for an ascii character", () => {
+    it("can't shrink 'a'", () => {
+      assertNoChange(dom.asciiChar(), () => true, "a");
+    });
+    it("can't shrink when all characters are used", () => {
+      repeatTest(arb.asciiChar(), (start) => {
+        assertNoChange(dom.asciiChar(), (c) => c === start, start);
+      });
+    });
+    it("shrinks an unused character to 'a'", () => {
+      assertShrinks(dom.asciiChar(), () => true, "Z", "a");
+    });
+    it("shrinks a used character to a lower one that works", () => {
+      assertShrinks(dom.asciiChar(), (s) => /[A-Z]/.test(s), "Z", "A");
+    });
+  });
+
+  describe("for a string", () => {
+    it("can't shrink an empty string", () => {
+      assertNoChange(dom.string(), () => true, "");
+    });
+    it("can't shrink when there's no alternative", () => {
+      repeatTest(arb.string(), (start) => {
+        assertNoChange(dom.string(), (s) => s === start, start);
+      }, { reps: 10 });
+    });
+    it("removes unused trailing characters", () => {
+      assertShrinks(dom.string(), (s) => s.startsWith("a"), "abc", "a");
+    });
+    it("sets unused characters to 'a'", () => {
+      assertShrinks(dom.string(), (s) => s.at(2) === "z", "xyz", "aaz");
+    });
+    it("removes unused leading characters", () => {
+      assertShrinks(dom.string(), (s) => s.endsWith("z"), "xyz", "z");
+    });
+  });
+
+  describe("for a sequence of options", () => {
+    const bits = arb.array(arb.int(0, 1), { length: { min: 1, max: 100 } });
+
+    it("removes any sequence of options", () => {
+      repeatTest(bits, (bits) => {
+        const seed = seedFrom(bits.map(() => bitReq), bits);
+        const gen = shrink(seed, acceptAll);
+        assert(gen !== undefined);
+        assertEquals(gen.picks.trimmed().replies, []);
+      });
+    });
+
+    it("removes everything except one option", () => {
+      const input = arb.from((pick) => {
+        const prefix = pick(bits);
+        const suffix = pick(bits);
+        return { prefix, suffix };
+      });
+      repeatTest(input, ({ prefix, suffix }) => {
+        const val = prefix.concat([1]).concat(suffix);
+        const seed = seedFrom(val.map((_) => bitReq), val);
+        const gen = shrink(seed, (arr) => arr.at(prefix.length) === 1);
+        assert(gen !== undefined);
+        const expected = Array(prefix.length).fill(0).concat(1);
+        assertEquals(gen.picks.trimmed().replies, expected);
+      });
+    });
+  });
+
+  describe("for a mult-step build script", () => {
+    const bit = Script.make("bit", (pick) => pick(PickRequest.bit));
+
+    const twoBits = bit.then("twoBits", (a, pick) => {
+      const b = pick(PickRequest.bit);
+      return [a, b];
+    });
+
+    it("can't shrink the default value", () => {
+      const seed = Gen.mustBuild(twoBits, [0, 0]);
+      const gen = shrink(seed, acceptAll);
+      assertEquals(gen.val, [0, 0]);
+    });
+
+    it("can shrink both bits to zero", () => {
+      const seed = Gen.mustBuild(twoBits, [1, 1]);
+      const gen = shrink(seed, acceptAll);
+      assertEquals(gen.val, [0, 0]);
+    });
+  });
+
+  function includesSubarray<T>(array: T[], subarray: T[]): boolean {
+    return array.some((_, i) =>
+      subarray.every((val, j) => val === array[i + j])
+    );
+  }
+
+  describe("for an array of ints", () => {
+    it("removes leading and trailing unused elements", () => {
+      const hay = arb.array(arb.int(1, 5), { length: { max: 10 } });
+      const example = arb.from((pick) => {
+        const prefix = pick(hay);
+        const suffix = pick(hay);
+        const needleSize = pick(arb.int(1, 3));
+        const needle = Array(needleSize).fill(6);
+        return { prefix, needle, suffix };
+      });
+      repeatTest(example, ({ prefix, needle, suffix }) => {
+        const input = prefix.concat(needle).concat(suffix);
+        assertShrinks(
+          dom.array(dom.int(1, 6)),
+          (a) => includesSubarray(a, needle),
+          input,
+          needle,
+        );
+      }, { reps: 100 });
+    });
+  });
+
+  describe("for an array of strings", () => {
+    it("removes all strings in an array", () => {
+      const input = arb.array(arb.string(), { length: { min: 1, max: 5 } });
+      repeatTest(input, (arr) => {
+        assertShrinks(dom.array(dom.string()), acceptAll, arr, []);
+      });
+    });
+
+    it("removes leading and trailing unused elements", () => {
+      const hayString = arb.string({ length: { max: 2 } });
+      const hay = arb.array(hayString, { length: { max: 10 } });
+      const example = arb.from((pick) => {
+        const prefix = pick(hay);
+        const suffix = pick(hay);
+        const needleSize = pick(arb.int(1, 3));
+        const needle = Array(needleSize).fill("<->");
+        return { prefix, needle, suffix };
+      });
+      repeatTest(example, ({ prefix, needle, suffix }) => {
+        const input = prefix.concat(needle).concat(suffix);
+        assert(!includesSubarray(prefix, needle));
+        assertShrinks(
+          dom.array(dom.string()),
+          (a) => includesSubarray(a, needle),
+          input,
+          needle,
+        );
+      }, { reps: 100 });
+    });
+
+    function repeatString(s: string, n: number): string {
+      let res = "";
+      for (let i = 0; i < n; i++) {
+        res += s;
+      }
+      return res;
+    }
+
+    it("shrinks each string to a given size", () => {
+      const input = arb.array(arb.string({ length: 20 }), { length: 10 });
+      const seed = generate(input, onePlayout(randomPicker(123)));
+      assert(seed !== filtered);
+      const gen = shrink(seed, () => true);
+      const expectedItem = repeatString("a", 20);
+      assertEquals(gen.val, Array(10).fill(expectedItem));
+    });
+  });
+
+  describe("for a filtered string", () => {
+    const filtered = dom.string().filter((s) =>
+      s.length > 0 && !s.startsWith("a")
+    );
+    it("can't shrink a default value", () => {
+      assertNoChange(filtered, () => true, "b");
+    });
+    it("shrinks non-default values", () => {
+      repeatTest(filtered.filter((s) => s !== "b"), (suffix, console) => {
+        assertShrinks(filtered, () => true, suffix, "b", console);
+      }, { reps: 100 });
+    });
+  });
+
+  describe("for a record", () => {
+    it("can't shrink an empty record", () => {
+      assertNoChange(dom.record({}), () => true, {});
+    });
+    const pair = dom.record({ a: dom.int32(), b: dom.string() });
+    it("can't shrink when there's no alternative", () => {
+      repeatTest(pair, ({ a, b }) => {
+        assertNoChange(
+          pair,
+          (r) => r.a === a && r.b === b,
+          { a, b },
+        );
+      }, { reps: 10 });
+    });
+    it("shrinks all fields to their minimums", () => {
+      repeatTest(pair, (start) => {
+        assertShrinks(pair, (_r) => true, start, { a: 0, b: "" });
+      }, { reps: 100 });
+    });
+    it("shrinks the first field if the second is held constant", () => {
+      repeatTest(pair, ({ a, b }) => {
+        assertShrinks(pair, (r) => r.b === b, { a, b }, { a: 0, b });
+      }, { reps: 10 });
+    });
+    it("shrinks the second field if the first is held constant", () => {
+      repeatTest(pair, ({ a, b }) => {
+        assertShrinks(pair, (r) => r.a === a, { a, b }, { a, b: "" });
+      }, { reps: 100 });
     });
   });
 });
