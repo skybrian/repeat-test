14dcb73d6f5fe1f9dcdef7ca30a6831f4c971f9c
Brian Slesinsky <brian-git@slesinsky.org>
2024-11-11 18:56:34 -0800

    tidy: move object and taggedUnion to rows.ts

---
+++ src/domains/rows.ts
@@ -0,0 +1,96 @@
+import type { PickifyFunction, PropShape } from "@/domain.ts";
+
+import * as arb from "@/arbs.ts";
+import { Domain, PropDomain } from "@/domain.ts";
+import { checkKeys } from "../options.ts";
+
+/** Options for {@link object}. */
+export type PropOpts = {
+  /** Turns on checking for extra properties. */
+  strict?: boolean;
+};
+
+/**
+ * A domain that matches an object when each of the given properties matches.
+ *
+ * Additional properties are normally allowed, unless the `strict` option is set.
+ *
+ * After being parsed or regenerated, the copy will only have the listed properties
+ * and the copy's prototype will be Object.prototype.
+ */
+export function object<T extends Record<string, unknown>>(
+  shape: PropShape<T>,
+  opts?: PropOpts,
+): PropDomain<T> {
+  const pickify: PickifyFunction = (val, sendErr) => {
+    if (!checkKeys(val, shape, sendErr, opts)) {
+      return undefined;
+    }
+
+    const out: number[] = [];
+    for (const key of Object.keys(shape)) {
+      const propVal = val[key as keyof typeof val];
+      const picks = shape[key].innerPickify(propVal, sendErr, key);
+      if (picks === undefined) return undefined;
+      out.push(...picks);
+    }
+    return out;
+  };
+
+  const build = arb.object<T>(shape).buildScript;
+
+  return new PropDomain(pickify, build, shape);
+}
+
+/**
+ * A domain that finds the first case where the given property matches, then
+ * tries to match the rest of the value.
+ */
+export function taggedUnion<T extends Record<string, unknown>>(
+  tagProp: string,
+  cases: PropDomain<T>[],
+): Domain<T> {
+  if (cases.length === 0) {
+    throw new Error("taggedUnion requires at least one case");
+  }
+
+  const tagPatterns: Domain<unknown>[] = [];
+  for (let i = 0; i < cases.length; i++) {
+    const c = cases[i];
+    const pattern = c.propAt(tagProp);
+    if (!pattern) {
+      throw new Error(`case ${i} doesn't have a '${tagProp}' property`);
+    }
+    tagPatterns.push(pattern);
+  }
+
+  const pickify: PickifyFunction = (val, sendErr, name) => {
+    if (val === null || typeof val !== "object") {
+      sendErr("not an object", val);
+      return undefined;
+    }
+
+    const hasTag: { [key in string]?: unknown } = val;
+    const actual = hasTag[tagProp];
+    if (typeof actual !== "string") {
+      sendErr(`'${tagProp}' property is not a string`, val);
+      return undefined;
+    }
+    for (let i = 0; i < cases.length; i++) {
+      const c = cases[i];
+      if (!tagPatterns[i].matches(actual)) {
+        continue; // tag didn't match
+      }
+      const picks = c.innerPickify(val, sendErr);
+      if (picks === undefined) {
+        return undefined; // rest of pattern didn't match
+      }
+      return [i, ...picks];
+    }
+    sendErr(`${tagProp}: "${actual}" didn't match any case in '${name}'`, val);
+  };
+
+  const build = arb.oneOf(...cases).with({ name: "taggedUnion" });
+
+  return Domain.make(build, pickify, { lazyInit: true });
+}
--- src/entrypoints/doms.ts
+++ src/entrypoints/doms.ts
@@ -6,4 +6,5 @@
 export * from "../domains/basics.ts";
 export * from "../domains/numbers.ts";
 export * from "../domains/strings.ts";
+export * from "../domains/rows.ts";
 export * from "../domains/tables.ts";
